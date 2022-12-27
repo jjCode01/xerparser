7599a4165706c096cb4740f885e7bd87e51b6566
Jesse Jones <code@seqmanagement.com>
2022-12-26 19:22:59 -0800

    No longer based on Pydantic BaseModel since all attributes are strings

--- xerparser/schemas/taskmemo.py
+++ xerparser/schemas/taskmemo.py
@@ -1,32 +1,30 @@
 # xerparser
 # taskmemo.py
 
-from pydantic import BaseModel, Field, validator
 import re
 from html_sanitizer import Sanitizer
+from typing import Any
 
 
-class TASKMEMO(BaseModel):
+class TASKMEMO:
     """A class to represent a note assigned to an activity"""
 
-    uid: str = Field(alias="memo_id")
-    memo: str = Field(alias="task_memo")
-    memo_type_id: str
-    proj_id: str
-    task_id: str
-    topic: str
-
-    class config:
-        arbitrary_types_allowed = True
-
-    @validator("memo", pre=True)
-    def sanitize_html(value) -> str:
-        sanitzer = Sanitizer()
-        memo = re.sub(r"(\u007F+)|(ï»¿)", "", value)
-        return sanitzer.sanitize(memo)
+    def __init__(self, **data: Any) -> None:
+        self.uid: str = data["memo_id"]
+        self.memo: str = _sanitize_html(data["task_memo"])
+        self.memo_type_id: str = data["memo_type_id"]
+        self.proj_id: str = data["proj_id"]
+        self.task_id: str = data["task_id"]
+        self.topic: str = data["topic"]
 
     def __eq__(self, __o: "TASKMEMO") -> bool:
         return self.topic == __o.topic
 
     def __hash__(self) -> int:
         return hash(self.topic)
+
+
+def _sanitize_html(value) -> str:
+    sanitzer = Sanitizer()
+    memo = re.sub(r"(\u007F+)|(ï»¿)", "", value)
+    return sanitzer.sanitize(memo)
