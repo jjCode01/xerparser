983c8e02617e7f29c676b06eb2e66dd87b9d7297
Jesse Jones <code@seqmanagement.com>
2022-12-26 19:09:03 -0800

    Refactor attribute names to be more consistent with other classes

--- xerparser/schemas/findates.py
+++ xerparser/schemas/findates.py
@@ -2,25 +2,25 @@
 # findates.py
 
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class FINDATES(BaseModel):
     """A class representing a Financial Period"""
 
-    fin_dates_id: str  # Unique ID
-    fin_dates_name: str  # Period Name
+    uid: str = Field(alias="fin_dates_id")  # Unique ID
+    name: str = Field(alias="fin_dates_name")  # Period Name
     start_date: datetime
     end_date: datetime
 
     def __eq__(self, __o: "FINDATES") -> bool:
         return all(
             (
-                self.fin_dates_name == __o.fin_dates_name,
+                self.name == __o.name,
                 self.start_date == __o.start_date,
                 self.end_date == __o.end_date,
             )
         )
 
     def __hash__(self) -> int:
-        return hash((self.fin_dates_name, self.start_date, self.end_date))
+        return hash((self.name, self.start_date, self.end_date))
