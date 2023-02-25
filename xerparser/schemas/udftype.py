0744668b735b82f6b92d33b2626c85871d8fa740
Jesse <code@seqmanagement.com>
2023-02-25 15:16:52 -0800

    Updated UDF Indicator Field to show emojis similar to what is shown in P6

--- xerparser/schemas/udftype.py
+++ xerparser/schemas/udftype.py
@@ -16,6 +16,12 @@ class UDFTYPE:
         FT_STATICTYPE = "Indicator"
         FT_TEXT = "Text"
 
+    class Indicators(Enum):
+        UDF_G1 = "🛑"
+        UDF_G2 = "⚠️"
+        UDF_G3 = "✅"
+        UDF_G4 = "⭐"
+
     def __init__(self, **data) -> None:
         self.uid: str = data["udf_type_id"]
         self.table: str = data["table_name"]
@@ -39,5 +45,8 @@ class UDFTYPE:
 
         if udf_type.type is UDFTYPE.FieldType.FT_INT:
             return int(data["udf_number"])
+
+        if udf_type.type is UDFTYPE.FieldType.FT_STATICTYPE:
+            return UDFTYPE.Indicators[data["udf_text"]].value
 
         return data["udf_text"]
