9a90223708f58d7e947a03639ad9678804d39e6e
Jesse <code@seqmanagement.com>
2023-02-28 10:31:14 -0800

    added eq and hash dunder methods to ERMHDR class

--- xerparser/schemas/ermhdr.py
+++ xerparser/schemas/ermhdr.py
@@ -26,3 +26,16 @@ class ERMHDR:
         self.date: datetime = datetime.strptime(args[1], "%Y-%m-%d")
         self.user: str = args[4]
         self.currency: str = args[7]
+
+    def __eq__(self, __o: "ERMHDR") -> bool:
+        return all(
+            (
+                self.version == __o.version,
+                self.date == __o.date,
+                self.user == __o.user,
+                self.currency == __o.currency,
+            )
+        )
+
+    def __hash__(self) -> int:
+        return hash((self.version, self.date, self.user, self.currency))
