96464c07e7c1220430dc3e8bb020c0d8f787a7bf
Jesse <code@seqmanagement.com>
2024-03-17 08:21:33 -0700

    Added type hints and docstrings

--- xerparser/schemas/_node.py
+++ xerparser/schemas/_node.py
@@ -1,32 +1,38 @@
+from typing import Self
+
+
 class Node:
     """A class to represent a node in a `Tree Data Structure`."""
 
     def __init__(self) -> None:
-        self._parent = None
-        self._children = []
+        self._parent: Self | None = None
+        self._children: list[Self] = []
 
-    def addChild(self, child) -> None:
+    def addChild(self, child: Self) -> None:
+        """Add child node"""
         if type(self) != type(child):
             raise TypeError(f"Expected {type(self)}; got {type(child)}")
         self._children.append(child)
 
     @property
-    def children(self) -> list:
+    def children(self) -> list[Self]:
+        """List of children nodes"""
         return self._children
 
     @property
-    def lineage(self) -> list:
+    def lineage(self) -> list[Self]:
         if not self._parent:
             return [self]
 
         return self._parent.lineage + [self]
 
     @property
-    def parent(self):
+    def parent(self) -> Self | None:
+        """Parent node"""
         return self._parent
 
     @parent.setter
-    def parent(self, value) -> None:
+    def parent(self, value: Self | None) -> None:
         if value is None:
             return
 
